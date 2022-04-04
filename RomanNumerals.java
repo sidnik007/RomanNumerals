394a5200359b7e973ed288841c42998663004415
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-04-04 09:17:17 +0530

    Add production code to pass failing test for 0 and empty String.

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -1,5 +1,13 @@
 public class RomanNumerals {
 
+    public static int parseRoman(final String roman) {
+        return 0;
+    }
+
+    public static String toRoman(final int number) {
+        return "";
+    }
+
     public static void main(final String... args) {
         assert 0 == parseRoman("");
         assert "".equals(toRoman(0));
