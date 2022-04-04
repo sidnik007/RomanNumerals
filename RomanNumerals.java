0b9632323bd1741b2f26d51cd94fc9b138b1d986
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-04-04 09:19:40 +0530

    Add failing tests for 1 and I.

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -11,5 +11,8 @@ public class RomanNumerals {
     public static void main(final String... args) {
         assert 0 == parseRoman("");
         assert "".equals(toRoman(0));
+
+        assert 1 == parseRoman("I");
+        assert "I".equals(toRoman(1));
     }
 }
